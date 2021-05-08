620875ed306b54efab2165a9959be9fa6b5dc7ba
Amanda Irazabal <amanda.irazabal@gmail.com>
2021-05-07 21:31:55 -0400

    Font styling

--- src/components/SkillIcon/SkillIcon.js
+++ src/components/SkillIcon/SkillIcon.js
@@ -18,7 +18,15 @@ const SkillIcon = (props) => {
   return (
     <div className={classes.iconHolder}>
       {icon}
-      <h3 className={classes.title}>{props.title}</h3>
+      <h3
+        style={{
+          fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI" Ubuntu, Cantarell,
+        "Helvetica Neue", sans-serif`
+        }}
+        className={classes.title}
+      >
+        {props.title}
+      </h3>
     </div>
   );
 };
